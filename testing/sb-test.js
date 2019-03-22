b00157893aa4be8a9d7504574e33672282151bd7
Ingar Helgesen <ingar.helgesen@t-fk.no>
2019-03-22 20:41:44 +0100

    More testing

--- testing/sb-test.js
+++ testing/sb-test.js
@@ -1,24 +1,32 @@
 require('dotenv').config()
-const msRest = require("@azure/ms-rest-js")
-const msRestAzure = require("@azure/ms-rest-azure-js")
-const msRestNodeAuth = require("@azure/ms-rest-nodeauth")
+const auth = require('../lib/authenticate')
 const armServiceBus = require("@azure/arm-servicebus")
-const armSubscriptions = require('@azure/arm-subscriptions');
-//const subscriptionId = process.env["AZURE_SUBSCRIPTION_ID"];
+const armResources = require('@azure/arm-resources');
 
 
 (async () => {
 
-  const creds = await msRestNodeAuth.interactiveLogin()
+  const authRes = await auth()
+  const creds = authRes.credentials
+  const subscriptionId = authRes.subscriptions[0].id
 
-  const subs = new armSubscriptions.SubscriptionClient(creds)
-  let subscriptionId = await subs.subscriptions.list()
-  subscriptionId = subscriptionId[0].subscriptionId
+  const resource = new armResources.ResourceManagementClient(creds, subscriptionId)
+  const resourceGroups = await resource.resourceGroups.list()
+  const resources = await resource.resources.list()
+
+  console.log(
+    resources.filter(resource => resource.type === 'Microsoft.ServiceBus/namespaces')
+    .map(res => res.name)
+  )
+  
+    
 
   const client = new armServiceBus.ServiceBusManagementClient(creds, subscriptionId);
   const queues = await client.queues.listByNamespace('tfkdevtest', 'tfkdevtest')
 
-  console.log(queues)
+  
+
+  console.log(queues.map(queue => queue.name))
 })().catch(err => {
   console.error(err)
 })
