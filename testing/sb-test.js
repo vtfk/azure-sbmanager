a1c1e2aec9c3e4c877aba167b000e2621f256f9d
Ingar Helgesen <ingar.helgesen@t-fk.no>
2019-03-24 18:23:51 +0100

    Getting all resGroups, namespaces and queues

--- testing/sb-test.js
+++ testing/sb-test.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 const auth = require('../lib/authenticate')
-const armServiceBus = require("@azure/arm-servicebus")
+const armServiceBus = require('@azure/arm-servicebus')
 const armResources = require('@azure/arm-resources');
 
 
@@ -10,24 +10,56 @@ const armResources = require('@azure/arm-resources');
   const creds = authRes.credentials
   const subscriptionId = authRes.subscriptions[0].id
 
-  const resource = new armResources.ResourceManagementClient(creds, subscriptionId)
-  const resourceGroups = await resource.resourceGroups.list()
-  const resources = await resource.resources.list()
+  const resourceMgmClient = new armResources.ResourceManagementClient(creds, subscriptionId)
+  // Get all resourcegroups
+  const resourceGroups = await resourceMgmClient.resourceGroups.list()
 
-  console.log(
-    resources.filter(resource => resource.type === 'Microsoft.ServiceBus/namespaces')
-    .map(res => res.name)
-  )
-  
-    
+  // Get all servicebus namespaces
+  const resources = await resourceMgmClient.resources.list()
+  const sbNamespaces = resources
+    .filter(resource => resource.type === 'Microsoft.ServiceBus/namespaces')
 
-  const client = new armServiceBus.ServiceBusManagementClient(creds, subscriptionId);
-  const queues = await client.queues.listByNamespace('tfkdevtest', 'tfkdevtest')
+  console.log('##########################')
+  console.log('Resourcegroups:')
+  console.log(resourceGroups.map(rg => rg.name))
+  console.log('##########################')
+  console.log('SB Namespaces:')
+  console.log(sbNamespaces.map(ns => ns.name))
+  console.log('##########################')
 
-  
+  const sbMgmClient = new armServiceBus.ServiceBusManagementClient(creds, subscriptionId);
 
-  console.log(queues.map(queue => queue.name))
+  let allQueues = {}
+
+  // TODO: Restructure to send over RG and NS info
+  // TODO: Support filtering for RG and NS
+
+  for (let i = 0; i < resourceGroups.length; i++) {
+    const rGroup = resourceGroups[i];
+    console.log('| R | ' + rGroup.name)
+    allQueues[rGroup.name] = {}
+
+    let namespaces = await resourceMgmClient.resources.listByResourceGroup(rGroup.name)
+    namespaces = namespaces.filter(
+      resource => resource.type === 'Microsoft.ServiceBus/namespaces'
+    )
+
+    for (let j = 0; j < namespaces.length; j++) {
+      const namespace = namespaces[j];
+      console.log('| N |-- ' + namespace.name)
+
+      let queues = await sbMgmClient.queues.listByNamespace(rGroup.name, namespace.name)
+      queues.map(queue => {console.log('| Q |---- ' + queue.name)})
+      allQueues[rGroup.name][namespace.name] = queues
+      
+    }
+
+  }
+
+  // const queues = await sbMgmClient.queues.listByNamespace('tfkdevtest', 'tfkdevtest')
+
+
+  console.log(allQueues)
 })().catch(err => {
   console.error(err)
 })
-
