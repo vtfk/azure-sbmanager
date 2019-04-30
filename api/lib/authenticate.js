f104ba30a75d3fad3fedb1eb6358584eec315689
Ingar Helgesen <ingar.helgesen@t-fk.no>
2019-04-30 14:31:49 +0200

    Split cli auth and prep for web auth

--- api/lib/authenticate.js
+++ api/lib/authenticate.js
@@ -1,4 +1,6 @@
 const msRestNodeAuth = require("@azure/ms-rest-nodeauth")
+const msJsAuth = require('@azure/ms-rest-js')
+
 
 //TODO: This will later be done in the webapp
 // https://github.com/azure/azure-sdk-for-node/blob/HEAD/Documentation/Authentication.md#for-azure-graph-sdk
@@ -6,9 +8,18 @@ const msRestNodeAuth = require("@azure/ms-rest-nodeauth")
 /**
  * Copy the code in console and navigate to the link, then log in.
  */
-module.exports = async () => {
+module.exports.cli = async () => {
   try {
     return msRestNodeAuth.interactiveLoginWithAuthResponse()
+  } catch (error) {
+    throw error
+  }
+}
+
+module.exports.web = async (token) => {
+  try {
+    const auth = new msJsAuth.TokenCredentials(token)
+    return auth.signRequest()
   } catch (error) {
     throw error
   }
